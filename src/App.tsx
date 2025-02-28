6aed9b45be932563cd9d65e9f97e14194ec0c153
Đỗ Minh Đức <minhducdo202@gmail.com>
2025-02-28 17:29:08 +0700

    1

---
+++ src/App.tsx
@@ -0,0 +1,95 @@
+import { ChangeEvent, useState } from "react";
+import Calc from "./components/BMI";
+
+const Status = {
+  height: "",
+  weight: "",
+};
+
+function App() {
+  const [status, setStatus] = useState(Status);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setStatus((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const handleClick = () => {
+    const heightInMeters = parseFloat(status.height) / 100;
+    const bmiValue = (
+      parseFloat(status.weight) /
+      (heightInMeters * heightInMeters)
+    ).toFixed(2);
+
+    switch (true) {
+      case parseFloat(bmiValue) < 15:
+        alert(`Your BMI is ${bmiValue}. You are Very severely underweight`);
+        break;
+      case parseFloat(bmiValue) >= 15 && parseFloat(bmiValue) < 16:
+        alert(`Your BMI is ${bmiValue}. You are Severely underweight`);
+        break;
+      case parseFloat(bmiValue) >= 16 && parseFloat(bmiValue) < 18.5:
+        alert(`Your BMI is ${bmiValue}. You are Underweight`);
+        break;
+      case parseFloat(bmiValue) >= 18.5 && parseFloat(bmiValue) < 25:
+        alert(`Your BMI is ${bmiValue}. You are Normal (healthy weight)`);
+        break;
+      case parseFloat(bmiValue) >= 25 && parseFloat(bmiValue) < 30:
+        alert(`Your BMI is ${bmiValue}. You are Overweight`);
+        break;
+      case parseFloat(bmiValue) >= 30 && parseFloat(bmiValue) < 35:
+        alert(`Your BMI is ${bmiValue}. You are Moderately obese`);
+        break;
+      case parseFloat(bmiValue) >= 35 && parseFloat(bmiValue) < 40:
+        alert(`Your BMI is ${bmiValue}. You are Severely obese`);
+        break;
+      case parseFloat(bmiValue) >= 40:
+        alert(`Your BMI is ${bmiValue}. You are Very severely obese`);
+    }
+
+    setStatus(Status);
+  };
+
+  return (
+    <div className="form">
+      <Calc />
+      <div className="weight-input">
+        <label htmlFor="height">Weight: </label>
+        <input
+          id="weight"
+          type="number"
+          min="30"
+          max="300"
+          placeholder="50"
+          value={status.weight}
+          onChange={handleChange}
+        />{" "}
+        kg
+      </div>
+      <div className="height-input">
+        <label htmlFor="height">Height: </label>
+        <input
+          id="height"
+          type="number"
+          min="100"
+          max="200"
+          placeholder="150"
+          value={status.height}
+          onChange={handleChange}
+        />{" "}
+        cm
+      </div>
+      <button
+        id="bmi-cal"
+        className="bmi-btn"
+        onClick={handleClick}
+        disabled={status.weight === "" || status.height === ""}
+      >
+        Calculate
+      </button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BMI.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Calc = () => {
+  return (
+    <div>
+      <h2>Hello</h2>
+      <h3>Hi</h3>
+    </div>
+  );
+};
+
+export default Calc;
